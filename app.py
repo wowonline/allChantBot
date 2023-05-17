24615cedf60c010379e7df97676ec76aec76a600
wowonline <wowonline.fit@mail.ru>
2023-05-17 21:44:18 +0300

    debug

--- app.py
+++ app.py
@@ -65,8 +65,9 @@ def bot_parse_queries(response):
         # containing env variables
         
         debug_chat_id = -943279534
-        db.add_chat_and_create_group_all(debug_chat_id, 'group', 'тест ботика')
-        bot_send_message(debug_chat_id, f"user {chat_member_username} was INITIALLY added to {chat_id}")
+        if (db.check_if_chat_is_new(chat_id)):
+            db.add_chat_and_create_group_all(debug_chat_id, 'group', 'тест ботика')
+            bot_send_message(debug_chat_id, f"user {chat_member_username} was INITIALLY added to {chat_id}")
         return
         
         # DATABASE PART
